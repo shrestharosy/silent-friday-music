d7bef0f589fd980fea53a45cb1f668d9f215c7b8
sanish <sanish.pradhananga@gmail.com>
2019-05-17 07:29:42 +0545

    [TEST]: this is test

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log('this is lint-staged test')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
