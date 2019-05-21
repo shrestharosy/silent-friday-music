c78e0347e5e07d64b150f4afc6ace97b4e5bb481
sanish <sanish.pradhananga@gmail.com>
2019-05-21 23:38:48 +0545

    [FEAT]: added main player component

--- extension/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Wrapper from './popup/wrapper';
-import './css/main.css';
-import './public';
-
-document.body.appendChild(Wrapper());
--- extension/src/public.ts
+++ extension/src/public.ts
@@ -1,1 +1,2 @@
 import '../public/icons/minion.png';
+import './css/main.css';
