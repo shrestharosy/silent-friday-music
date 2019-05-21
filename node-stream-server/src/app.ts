bfa895271510c3adf78c1ad701cca1f10040a910
sanish <sanish.pradhananga@gmail.com>
2019-05-21 07:34:23 +0545

    [REFACTOR]: separated app and server

--- node-stream-server/src/app.ts
+++ node-stream-server/src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import ytdl from 'ytdl-core';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('This is test')
+  res.send('This is test');
 });
 
-app.listen(3002, () => {
-  console.log('Server is running..')
-})
+app.get('/test', (req, res) => {
+  const youtubeStream = ytdl('', { filter: 'audioonly' });
+  res.set({
+    'Content-Type': 'audio/mpeg',
+  });
+  youtubeStream.on('data', chunks => {
+    console.log(chunks);
+  });
+  res.send();
+});
+
+export default app;
