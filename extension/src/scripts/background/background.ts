cd760aecf286aab4d1fb9965e0305671c79dffa9
Rosy <rosyshrestha028@gmail.com>
2019-05-22 17:48:05 +0545

    [FEAT] : Stream audio in background script

--- extension/src/scripts/background/background.ts
+++ extension/src/scripts/background/background.ts
@@ -1,24 +1,53 @@
-function createAudioElement() {
-  console.log('roz', document);
+import axios from '../../popup/utils/axios';
+
+function createAudioElement(url: string) {
   const audioElement: HTMLAudioElement = document.createElement('audio');
   audioElement.setAttribute('preload', 'auto');
 
-  const source1: HTMLSourceElement = document.createElement('source');
-  source1.type = 'audio/mpeg';
-  source1.src = 'http://lezotre.free.fr/Mp3/disco.mp3';
-  audioElement.appendChild(source1);
+  const source: HTMLSourceElement = document.createElement('source');
+  source.type = 'audio/mpeg';
+  source.src = url;
+  audioElement.appendChild(source);
 
   return audioElement;
 }
 
+interface IBroadcastResponse {
+  avatar: string;
+  name: string;
+  streamUrl: string;
+}
+
+async function loadAudio(url: string) {
+  const { avatar, name, streamUrl } = await axios
+    .post<IBroadcastResponse>('/broadcast', {
+      requestUrl: url,
+    })
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      throw error;
+    });
+
+  const audioElement = createAudioElement(streamUrl);
+  audioElement.play();
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  const audioElement = createAudioElement();
-  if (request.type == 'PLAY') {
-    audioElement.load;
-    audioElement.play();
-  }
-  if (request.type == 'PAUSE') {
-    debugger;
-    audioElement.pause();
+  const url = 'http://lezotre.free.fr/Mp3/disco.mp3';
+  const audioElement = createAudioElement(url);
+
+  switch (request.type) {
+    case 'LOAD_AUDIO': {
+      loadAudio(request.data);
+      break;
+    }
+    case 'PLAY': {
+      audioElement.load;
+      audioElement.play();
+    }
+    default:
   }
 });
