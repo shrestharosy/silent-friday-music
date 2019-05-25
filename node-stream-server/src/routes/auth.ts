47fe2d0ed8821557673f6e3486b417605329f083
pragyakar <pragyakar75@gmail.com>
2019-05-26 00:48:14 +0545

    [FEAT] : Added routes for auth

---
+++ node-stream-server/src/routes/auth.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import * as authServices from '../services/authServices';
+import * as jwtServices from '../utils/jwt';
+
+const authRouter = Router();
+
+// TODO: Add status codes and proper formatting to responses
+ 
+authRouter.post('/login', async (req, res, next) => {
+  try {
+    if(!req.body.token) {
+      res.send('Token missing');
+      throw new Error('Token missing');
+    }
+
+    const userData = await authServices.getUserDataFromToken(req.body.token);
+    const accessToken = jwtServices.generateAccessToken(userData);
+    const refreshToken = jwtServices.generateRefreshToken(userData);
+    
+    // TODO: check if user exists in DB 
+    // TODO: create new user if user does not exist
+
+    const response = {
+      accessToken,
+      refreshToken,
+      // TODO: return user data from DB along with the tokens in response
+    }
+    res.send(response);
+  } catch (error) {
+    throw new Error(error);
+  } 
+});
+
+authRouter.post('/logout', (req, res, next) => {
+  res.send('logout');
+});
+
+authRouter.post('/refresh-token', (req, res, next) => {
+  res.send('Refresh Token');
+});
+
+export default authRouter;
