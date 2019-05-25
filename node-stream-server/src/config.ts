6aeafcca81f8cd805d159a201420817817df2002
pragyakar <pragyakar75@gmail.com>
2019-05-25 23:42:41 +0545

    [CHORE] : Added jwt functions

--- node-stream-server/src/config.ts
+++ node-stream-server/src/config.ts
@@ -2,7 +2,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  authRedirect: process.env.AUTH_REDIRECT
+  googleAuth: {
+    googleClientId: process.env.GOOGLE_CLIENT_ID || 'abc123',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'abc123',
+    authRedirect: process.env.AUTH_REDIRECT
+  },
+  auth: {
+    saltRounds: process.env.SALT_ROUNDS,
+    accessTokenDuration: process.env.ACCESS_TOKEN_DURATION, 
+    refreshTokenDuration: process.env.REFRESH_TOKEN_DURATION,
+    accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY || 'abc123',
+    refreshTokenSecretKey: process.env.REFRESH_TOKEN_SECRET_KEY || 'abc123'
+  },
+  timeout: process.env.TIEMOUT
 }
+
--- node-stream-server/src/services/authServices.ts
+++ node-stream-server/src/services/authServices.ts
@@ -2,17 +2,17 @@ import { OAuth2Client } from 'google-auth-library';
 import config from '../config';
 
 export async function getUserDataFromToken(token: string) {
-  const googleClient = new OAuth2Client(config.googleClientId, '', '');
+  const googleClient = new OAuth2Client(config.googleAuth.googleClientId, '', '');
 
   return new Promise((resolve, reject) => {
     if (!token) {
       throw new Error('Token missing');
     }
     googleClient
-      .verifyIdToken({ idToken: token, audience: 'google client Id here'})
+      .verifyIdToken({ idToken: token, audience: config.googleAuth.googleClientId})
       .then((login: any) => {
         const payload = login.getPayload();
-        if(payload['aud'] === config.googleClientId) {
+        if(payload['aud'] === config.googleAuth.googleClientId) {
           resolve({
             userId: payload.sub,
             email: payload.email,
