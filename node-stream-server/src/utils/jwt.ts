162f63ac4014d95b6fd927ee565084c187704f7b
Rosy <rosyshrestha028@gmail.com>
2019-06-14 18:42:09 +0545

    [FIX] : Fix return types

--- node-stream-server/src/utils/jwt.ts
+++ node-stream-server/src/utils/jwt.ts
@@ -5,11 +5,11 @@ interface ITokenPayload {
   id: string;
 }
 
-export function generateAccessToken(data: ITokenPayload) {
-  return jwbt.sign(data, config.auth.accessTokenSecretKey, { expiresIn: config.auth.accessTokenDuration });  
+export function generateAccessToken(data: ITokenPayload | string) {
+  return jwbt.sign(data, config.auth.accessTokenSecretKey, { expiresIn: config.auth.accessTokenDuration });
 }
 
-export function generateRefreshToken(data: ITokenPayload) {
+export function generateRefreshToken(data: ITokenPayload | string) {
   return jwbt.sign(data, config.auth.refreshTokenSecretKey, { expiresIn: config.auth.refreshTokenDuration });
 }
 
@@ -22,5 +22,12 @@ export function verifyRefreshToken(token: string) {
 }
 
 export function decodeToken(token: string) {
-  return jwbt.decode(token);
+  const jwtDecoded = jwbt.decode(token);
+  let decodedToken = null;
+  if (typeof jwtDecoded === 'object') {
+    decodedToken = jwtDecoded as ITokenPayload;
+  } else {
+    decodedToken = jwtDecoded;
+  }
+  return decodedToken;
 }
