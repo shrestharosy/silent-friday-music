30216bcb94755b4f0ad853a05aa97935badfb302
sanish <sanish.pradhananga@gmail.com>
2019-06-16 17:36:45 +0545

    [FEAT]: added respective action creators

---
+++ extension/src/actionCreators/actionCreator.ts
@@ -0,0 +1,26 @@
+import * as ActionTypes from './actionCreator.d';
+import * as actionConstants from 'src/constants/actions';
+import { IFillBroadcastActionPayload } from 'src/scripts/background/reducers/broadcast';
+import { IFillRoomActionPayload } from 'src/scripts/background/reducers/room';
+import { IFillProfileActionPayload } from 'src/scripts/background/reducers/profile';
+
+export function fillBroadcastAction(payload: IFillBroadcastActionPayload): ActionTypes.FillBroadcastActionType {
+  return {
+    type: actionConstants.FILL_BROADCAST_ACTION,
+    payload,
+  };
+}
+
+export function fillRoomAction(payload: IFillRoomActionPayload): ActionTypes.FillRoomActionType {
+  return {
+    type: actionConstants.FILL_ROOM_ACTION,
+    payload,
+  };
+}
+
+export function fillProfileAction(payload: IFillProfileActionPayload): ActionTypes.FillProfileActionType {
+  return {
+    type: actionConstants.FILL_PROFILE_ACTION,
+    payload,
+  };
+}
