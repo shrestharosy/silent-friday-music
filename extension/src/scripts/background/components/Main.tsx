3eedb5ecf7dcae0554d2df94d932cae6e789c585
sanish <sanish.pradhananga@gmail.com>
2019-06-23 17:02:21 +0545

    [FEAT]: completed phantom player and added now playing

--- extension/src/scripts/background/components/Main.tsx
+++ extension/src/scripts/background/components/Main.tsx
@@ -2,21 +2,31 @@ import * as React from 'react';
 
 import socketService from 'src/services/socket.service';
 
-import Audio from './Audio';
+import NowPlaying from './NowPlaying';
 import TimeKeeper from './Phantom/TimeKeeper';
+import { connect } from 'react-redux';
+import { IReduxState } from '../reducers/rootReducer';
+import { IActiveReduxState, AvailableComponents } from '../reducers/active';
 
-class Main extends React.Component<{}> {
+const mapStateToProps = ({ active }: IReduxState) => ({ active });
+
+interface IMainProps {
+  active: IActiveReduxState;
+}
+
+class Main extends React.Component<IMainProps> {
   componentDidMount() {
     socketService.getIOInstance();
   }
   render() {
+    const { active } = this.props;
     return (
       <React.Fragment>
-        {/* <Audio url={''} /> */}
+        {active.component === AvailableComponents.ROOM_DETAILS && <NowPlaying roomId={active.id} />}
         <TimeKeeper />
       </React.Fragment>
     );
   }
 }
 
-export default Main;
+export default connect(mapStateToProps)(Main);
