da8ee8dd9851163e5aeefbafa86be152965d65ee
Rosy <rosyshrestha028@gmail.com>
2019-06-25 08:47:41 +0545

    [SFM-26] : [CHORE] : Filter users from master and validate create room form

--- extension/src/popup/components/Rooms/Form.tsx
+++ extension/src/popup/components/Rooms/Form.tsx
@@ -4,40 +4,98 @@ import { bindActionCreators, Dispatch } from 'redux';
 import { Form, InjectedFormikProps, withFormik, FormikBag } from 'formik';
 import Select from 'react-select';
 
-import { fillActiveAction, fetchUsers } from 'src/actionCreators/actionCreator';
+import { fetchUsers } from 'src/actionCreators/actionCreator';
+import { IUser } from '../../../sagas/user/effects';
+import { IReduxState } from 'src/scripts/background/reducers/rootReducer';
+import { IProfileReduxState } from 'src/scripts/background/reducers/profile';
 
 interface ICreateRoomFormProps {
+  profile: IProfileReduxState;
   fetchUsers: typeof fetchUsers;
 }
 
-const SKUOperatorOptions = [
-  { value: 'EQUAL', label: '=' },
-  { value: 'GREATER_THAN', label: '>' },
-  { value: 'LESS_THAN', label: '<' },
-];
-
 interface IFormValues {
   roomName: string;
+  usersList: Array<IOptionsProps>;
+  errorInRoomName: string;
+  errorInUsersList: string;
 }
 
-class CreateRoomForm extends React.Component<InjectedFormikProps<ICreateRoomFormProps, IFormValues>, {}> {
+interface ICreateRoomFormState {
+  isLoading: boolean;
+  options: Array<IOptionsProps>;
+}
+
+interface IOptionsProps {
+  value: string;
+  label: string;
+}
+
+class CreateRoomForm extends React.Component<
+  InjectedFormikProps<ICreateRoomFormProps, IFormValues>,
+  ICreateRoomFormState
+> {
+  constructor(props: Readonly<InjectedFormikProps<ICreateRoomFormProps, IFormValues>>) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      options: [],
+    };
+  }
+
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     this.props.setFieldValue('roomName', value.trim());
   };
 
-  getOptionsAsync = async (imput: string) => {
+  getOptionsAsync = async (input: string) => {
+    this.setState({
+      isLoading: true,
+    });
+    // const formattedInput = input ? input : '';
     const users = await new Promise((resolve, reject) => {
       this.props.fetchUsers(resolve, reject);
+    })
+      .then((users: Array<IUser>) => {
+        return users;
+      })
+      .catch(error => {
+        throw error;
+      });
+
+    const filteredUsers: Array<IUser> = users.filter((user: IUser) => {
+      return user.name !== this.props.profile.name;
     });
+
+    const mappedOptions: Array<IOptionsProps> = filteredUsers.map((user: IUser) => {
+      return { value: user.name, label: user.name };
+    });
+
+    this.setState({
+      options: mappedOptions ? mappedOptions : [],
+      isLoading: false,
+    });
+  };
+
+  // handleInputChange = (input: string) => {
+  //   this.getOptionsAsync(input);
+  // };
+
+  handleUserSelect = (value: Array<IOptionsProps>) => {
+    value = value ? value : [];
+    this.props.setFieldValue('usersList', value);
   };
 
   render() {
     const {
-      values: { roomName },
+      values: { roomName, usersList },
       dirty,
       isSubmitting,
+      errors,
+      touched,
     } = this.props;
+
+    const { isLoading, options } = this.state;
     return (
       <Form>
         <input
@@ -50,20 +108,26 @@ class CreateRoomForm extends React.Component<InjectedFormikProps<ICreateRoomForm
           placeholder={'Enter Room Name'}
         />
 
+        <span>{errors && touched.roomName && errors.errorInRoomName}</span>
+
         <Select
-          //   id={name}
-          //   name={name}
-          className={'select-custom'}
-          classNamePrefix={'select-active'}
+          id={'usersList'}
+          name={'usersList'}
+          // className={'select-custom'}
+          // classNamePrefix={'select-active'}
           placeholder={'Add members'}
-          options={SKUOperatorOptions}
-          onChange={this.handleChange}
+          isLoading={isLoading}
+          options={options}
+          onChange={this.handleUserSelect}
           onMenuOpen={() => this.getOptionsAsync('')}
-          //   value={selectedValue}
+          // onInputChange={this.handleInputChange}
+          value={usersList}
           isSearchable={true}
           isClearable={true}
           isMulti={true}
         />
+
+        <span>{errors && touched.usersList && errors.errorInUsersList}</span>
         <button
           type="submit"
           disabled={!dirty || isSubmitting}
@@ -81,17 +145,30 @@ const WrappedForm = withFormik<ICreateRoomFormProps, IFormValues>({
   mapPropsToValues() {
     return {
       roomName: '',
+      usersList: [],
+      errorInRoomName: '',
+      errorInUsersList: '',
     };
   },
   validate: (values: IFormValues) => {
+    let errors = { errorInRoomName: '', errorInUsersList: '' };
     if (values.roomName.length === 0) {
-      return;
+      errors.errorInRoomName = 'Please enter a room name';
+    }
+    if (values.usersList.length === 0) {
+      errors.errorInUsersList = 'Please select at least one user';
+    }
+    if (Object.keys(errors).length) {
+      return errors;
     }
   },
   handleSubmit(values: IFormValues, actions: FormikBag<{}, {}>) {
+    // inside action
     actions.setSubmitting(false);
   },
 })(CreateRoomForm);
+
+const mapStateToProps = ({ profile }: IReduxState) => ({ profile });
 
 const mapDispatchToProps = (
   dispatch: Dispatch<{
@@ -100,7 +177,7 @@ const mapDispatchToProps = (
 ) => bindActionCreators({ fetchUsers }, dispatch);
 
 const ConnectedForm = connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(WrappedForm);
 
--- extension/src/sagas/user/effects.ts
+++ extension/src/sagas/user/effects.ts
@@ -3,9 +3,15 @@ import { call, put } from 'redux-saga/effects';
 import { fetchUsersAPI } from './apis';
 import * as ActionCreatorsTypes from '../../actionCreators/actionCreator.d';
 
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+}
+
 export function* fetchUsersEffect(action: ActionCreatorsTypes.FetchUsersAction) {
   try {
-    const users = yield call(fetchUsersAPI);
+    const users: Array<IUser> = yield call(fetchUsersAPI);
     if (action.resolve) {
       action.resolve(users);
     }
