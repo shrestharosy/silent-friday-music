6dcc47c29d72556d61be26b2910dd98a93fcf3ce
Sanish Pradhananga <sanish.pradhananga@gmail.com>
2019-06-26 16:45:23 +0545

    [REFACTOR]: refactor listeners

--- extension/src/scripts/background/components/SocketHandler.tsx
+++ extension/src/scripts/background/components/SocketHandler.tsx
@@ -2,39 +2,66 @@ import * as React from 'react';
 import { IProfileReduxState } from '../reducers/profile';
 import getSocketInstance, { ISocketMessage } from 'src/services/socket.service';
 import { IRoomReduxState } from '../reducers/room';
-import { REQUEST_TO_BE_MASTER, BROADCAST_MASTER_SELECTION_REQUEST } from 'src/constants/socket';
+import {
+  REQUEST_TO_BE_MASTER,
+  BROADCAST_MASTER_SELECTION_REQUEST,
+  CHECK_MASTER_IS_ONLINE,
+  ACK_MASTER_IS_ONLINE,
+} from 'src/constants/socket';
 import { IReduxState } from '../reducers/rootReducer';
 import { connect } from 'react-redux';
+import { IActiveReduxState } from '../reducers/active';
 
 interface ISocketHandlerProps {
   profile: IProfileReduxState;
   room: IRoomReduxState;
+  active: IActiveReduxState;
 }
 
-const mapStateToProps = ({ profile, room }: IReduxState) => ({ profile, room });
+const mapStateToProps = ({ profile, room, active }: IReduxState) => ({ profile, room, active });
 
 export class SocketHandler extends React.Component<ISocketHandlerProps> {
+  componentDidMount() {
+    this.initListeners();
+  }
+  initListeners = () => {
+    const { id: roomId } = this.props.active;
+
+    const ioInstance = getSocketInstance().getIOInstance();
+    console.log('init socket...', this.props);
+    ioInstance.on(roomId, (message: ISocketMessage) => {
+      if (message.type === BROADCAST_MASTER_SELECTION_REQUEST && this.props.room.master !== this.props.profile._id) {
+        ioInstance.emit(JSON.stringify({ type: REQUEST_TO_BE_MASTER, roomId }), {
+          receiverId: 'Node',
+          message: {
+            type: REQUEST_TO_BE_MASTER,
+            payload: {
+              userId: this.props.profile._id,
+            },
+          },
+        });
+      } else if (message.type === CHECK_MASTER_IS_ONLINE) {
+        console.log(message);
+        ioInstance.emit(JSON.stringify({ type: ACK_MASTER_IS_ONLINE, roomId }), {
+          receiverId: 'Node',
+          message: {
+            type: ACK_MASTER_IS_ONLINE,
+            payload: {
+              userId: this.props.profile._id,
+              roomId,
+            },
+          },
+        });
+      }
+    });
+  };
+
   componentDidUpdate(prevProps: Readonly<ISocketHandlerProps>) {
-    const { _id: roomId } = this.props.room;
-    const { _id: prevRoomId } = prevProps.room;
+    const { id: roomId } = this.props.active;
+    const { id: prevRoomId } = prevProps.active;
 
     if (prevRoomId !== roomId) {
-      const ioInstance = getSocketInstance().getIOInstance();
-      console.log('init socket...', this.props);
-      ioInstance.on(roomId, (message: ISocketMessage) => {
-        if (message.type === BROADCAST_MASTER_SELECTION_REQUEST && this.props.room.master !== this.props.profile._id) {
-          console.log(message, JSON.stringify({ type: REQUEST_TO_BE_MASTER, roomId }));
-          ioInstance.emit(JSON.stringify({ type: REQUEST_TO_BE_MASTER, roomId }), {
-            receiverId: 'Node',
-            message: {
-              type: REQUEST_TO_BE_MASTER,
-              payload: {
-                userId: this.props.profile._id,
-              },
-            },
-          });
-        }
-      });
+      this.initListeners();
     }
   }
 
